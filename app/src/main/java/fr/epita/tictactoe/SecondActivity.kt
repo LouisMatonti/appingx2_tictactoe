a53f0ec84368c3f0da3b013c7d91a4684298d4f3
LouisMatonti <44303751+LouisMatonti@users.noreply.github.com>
2018-12-03 17:26:37 +0100

    Last Commit

--- app/src/main/java/fr/epita/tictactoe/SecondActivity.kt
+++ app/src/main/java/fr/epita/tictactoe/SecondActivity.kt
@@ -4,10 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.game.*
 
-class SecondActivity : AppCompatActivity() {
+
+class SecondActivity : AppCompatActivity(), View.OnClickListener {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +17,29 @@ class SecondActivity : AppCompatActivity() {
         /*val Intent = intent
         val PlaName = Intent.getStringExtra("Name")
         val PlayerName = this.findViewById<TextView>(R.id.textname)
-        PlayerName.text = "O (Louis Matonti)"
-        val Guest = findViewById<TextView>(R.id.Guest)
-        Guest.text = "X (GUEST)"*/
+        PlayerName.text = "O ($PlaName)"*/
+
+        button1.setOnClickListener(this@SecondActivity)
+        button2.setOnClickListener(this@SecondActivity)
+        button3.setOnClickListener(this@SecondActivity)
+        button4.setOnClickListener(this@SecondActivity)
+        button5.setOnClickListener(this@SecondActivity)
+        button6.setOnClickListener(this@SecondActivity)
+        button7.setOnClickListener(this@SecondActivity)
+        button8.setOnClickListener(this@SecondActivity)
+        button9.setOnClickListener(this@SecondActivity)
+
+        var tour = 0
+        var text = "0"
+        if(tour % 2 == 0)
+            text = "O"
+        else
+            text = "X"
+        while (tour < 9)
+        {
+            tour = tour + 1
+        }
+
     }
 
 }
